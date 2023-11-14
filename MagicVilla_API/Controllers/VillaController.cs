8d99faaf4018e3a2f149846e24d1af6f11ef7b69
Alfre Martinez <alfrermtmadrid@gmail.com>
2023-11-14 10:49:35 -0500

    DbContext - Inyeccion de Dependencia

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,207 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        private readonly ILogger<VillaController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ILogger<VillaController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            _logger.LogInformation("Obtener las Villas");
+            return Ok(_context.Villas.ToList());
+        }
+
+        [HttpGet("id", Name = "GetVilla")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                _logger.LogError("Error al traer Villa con Id " + id);
+                return BadRequest();
+            }
+
+            var villa = _context.Villas.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa); 
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public ActionResult<VillaDto> CrearVilla([FromBody] VillaDto villaDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_context.Villas.FirstOrDefault(v => v.Nombre.ToLower() ==  villaDto.Nombre.ToLower()) != null)
+            {
+                ModelState.AddModelError("NombreExiste", "La Villa con ese Nombre ya existe!");
+                return BadRequest(ModelState);
+            }
+
+            if (villaDto == null)
+            {
+                return BadRequest();
+            }
+
+            if (villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            Villa modelo = new()
+            {
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                MetrosCuadrados = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                Amenidad = villaDto.Amenidad,
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now,
+            };
+
+            _context.Villas.Add(modelo);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetVilla", new { id = villaDto.Id}, villaDto);
+        }
+
+        [HttpDelete("id")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id  == 0)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var villa = _context.Villas.FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Villas.Remove(villa);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("id")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null || id !=  villaDto.Id)
+            {
+                return BadRequest();
+            }
+
+            Villa model = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                MetrosCuadrados = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                Amenidad = villaDto.Amenidad,
+                FechaActualizacion = DateTime.Now,
+            };
+
+            _context.Villas.Update(model);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("id")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = _context.Villas.AsNoTracking().FirstOrDefault(v => v.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            VillaDto villaDto = new()
+            {
+                Id = villa.Id,
+                Nombre = villa.Nombre,
+                Detalle = villa.Detalle,
+                ImagenUrl = villa.ImagenUrl,
+                Ocupantes = villa.Ocupantes,
+                MetrosCuadrados = villa.Ocupantes,
+                Tarifa = villa.Tarifa,
+                Amenidad = villa.Amenidad,
+            };
+            
+            patchDto.ApplyTo(villaDto, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                MetrosCuadrados = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                Amenidad = villaDto.Amenidad,
+                FechaActualizacion = DateTime.Now
+            };
+
+            _context.Villas.Update(modelo);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ MagicVilla_API/Migrations/20231113185913_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la Villa...", new DateTime(2023, 11, 13, 13, 59, 13, 458, DateTimeKind.Local).AddTicks(934), new DateTime(2023, 11, 13, 13, 59, 13, 458, DateTimeKind.Local).AddTicks(922), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la Villa...", new DateTime(2023, 11, 13, 13, 59, 13, 458, DateTimeKind.Local).AddTicks(938), new DateTime(2023, 11, 13, 13, 59, 13, 458, DateTimeKind.Local).AddTicks(937), "", 40, "Premium Vista a la Piscina", 4, 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
